c388046a4775dc8d54b304f3464211b0f14b1222
yologithup <18821769219@163.com>
2020-04-21 12:06:02 +0800

    -

---
+++ Tangshi-Server2（后端）/src/test/java/Main/MainTest.java
@@ -0,0 +1,16 @@
+package Main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author yolo
+ * @date 2020/4/10-16:40
+ */
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
